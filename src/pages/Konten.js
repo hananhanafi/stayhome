2a8b20311185e8aa91d34be31841d977cf5075a2
hananhanafi <hananhanafi@mail.ugm.ac.id>
2020-04-14 19:48:44 +0700

    Myth buster covid-19 content

--- src/pages/Konten.js
+++ src/pages/Konten.js
@@ -86,18 +86,46 @@ class Konten extends Component{
                         </div>
                     </div>
                 </div>
-                
-                {/* <div className="row">
+
+                <div className="row">
                     <div className="col-12">
-                        <div className=" text-center my-5 bg-white p-5" style={{borderRadius:"20px"}}>
-                            <h2>Mitos</h2>
+                        <div className=" text-center my-5 bg-white p-md-5 pb-2 p-2" style={{borderRadius:"20px"}}>
+                            <h2>Mitos & Fakta</h2>
                             <hr/>
+                            <div className="row ">
+                                <div className="col-md-6 col-12 p-md-5 pb-2 ">
+                                    <img className="w-100 rounded-lg" src={require('../assets/img/post/post-01.png')} alt="post1"/>
+                                </div>
+                                <div className="col-md-6 col-12 p-md-5 pb-2 ">
+                                    <img className="w-100 rounded-lg" src={require('../assets/img/post/post-02.png')} alt="post1"/>
+                                </div>
+                                <div className="col-md-6 col-12 p-md-5 pb-2 ">
+                                    <img className="w-100 rounded-lg" src={require('../assets/img/post/post-03.png')} alt="post1"/>
+                                </div>  
+                                <div className="col-md-6 col-12 p-md-5 pb-2 ">
+                                    <img className="w-100 rounded-lg" src={require('../assets/img/post/post-04.png')} alt="post1"/>
+                                </div>  
+                                <div className="col-md-6 col-12 p-md-5 pb-2 ">
+                                    <img className="w-100 rounded-lg" src={require('../assets/img/post/post-05.png')} alt="post1"/>
+                                </div>  
+                                <div className="col-md-6 col-12 p-md-5 pb-2 ">
+                                    <img className="w-100 rounded-lg" src={require('../assets/img/post/post-06.png')} alt="post1"/>
+                                </div>  
+                                <div className="col-md-6 col-12 p-md-5 pb-2 ">
+                                    <img className="w-100 rounded-lg" src={require('../assets/img/post/post-07.png')} alt="post1"/>
+                                </div>  
+                                <div className="col-md-6 col-12 p-md-5 pb-2 ">
+                                    <img className="w-100" src={require('../assets/img/post/post-08.png')} alt="post1"/>
+                                </div>  
+                            </div>
                             
                         </div>
                     </div>
                 </div>
+                
+                
 
-                <div className="row">
+                {/* <div className="row">
                     <div className="col-12">
                         <div className=" text-center my-5 bg-white p-5" style={{borderRadius:"20px"}}>
                             <h2>Saran</h2>
@@ -107,6 +135,10 @@ class Konten extends Component{
                     </div>
                 </div> */}
                 
+            </div>
+
+            <div className="container">
+            
             </div>
 
 
